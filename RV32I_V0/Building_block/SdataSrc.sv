c8ae5ee21cee79abf3aa58fc21b0d769158e42fa
nvh013015 <nvh013015@gmail.com>
2026-04-27 01:00:09 +0700

    toi bi ngu

--- RV32I_V0/Building_block/SdataSrc.sv
+++ RV32I_V0/Building_block/SdataSrc.sv
@@ -5,25 +5,35 @@ module SdataSrc (
     input logic [31:0] rs1,
     input logic [31:0] rs2,
     input logic [31:0] imm,
-    input logic [3:0] SdataSrcControl,
+    input logic [1:0] SdataSrcA,
+    input logic [1:0] SdataSrcB,
     output logic [31:0] writeData,
     output logic [31:0] ALUsrcA,
     output logic [31:0] ALUsrcB
 );
-    logic[31:0] Data1,
-    always_ff @( posedge clock ) begin : blockName
+    logic[31:0] Data1;
+    always_ff @( posedge clock ) begin 
         Data1 <= rs1;
-        Writeback <= rs2;
+        writeData <= rs2;
     end
-    always_comb begin : blockName
-        case (SdataSrcControl)
-            4'b0000: writeData = Data1;           // rs1
-            4'b0001: writeData = Writeback;       // rs2
-            4'b0010: writeData = imm;             // immediate
-            4'b0011: writeData = newPC;          // newPC
-            4'b0100: writeData = oldPC;          // oldPC
-            default: writeData = 32'b0;          // Mặc định trả về 0 nếu không khớp
+
+    always_comb begin
+        case (SdataSrcA)
+            2'b00: ALUsrcA = newPC;
+            2'b01: ALUsrcA = oldPC;
+            2'b10: ALUsrcA = Data1; 
+            default: ALUsrcA = 0;
         endcase
     end
+    
+    always_comb begin 
+        case (SdataSrcB)
+            2'b00: ALUsrcB = writeData;
+            2'b01: ALUsrcB = imm;
+            2'b10: ALUsrcB = 32'd4; 
+            default: ALUsrcB = 0;
+        endcase
+    end
+
 
 endmodule
